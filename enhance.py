36210e93cc82531e8caee7c5f9d5f73153a15b1f
khoa16122004 <trannhatkhoacm1612@gmail.com>
2024-04-25 22:02:12 +0700

    "continue_code"

--- enhance.py
+++ enhance.py
@@ -22,3 +22,12 @@ def gammaTransformed(image, ld = 25, ep=1, gam = 0.5):
     transformed_image = ld * (image + ep)**gam 
     transformed_image = np.clip(transformed_image, 0, 255).astype(np.uint8)
     return transformed_image
+
+
+img_path = r"D:\AI\CV\CS231_Low-light-Enhancement-in-Classical-Computer-Vision-Tasks\image_test\2015_05025.jpg"
+img = cv2.imread(img_path)
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = logarithmTransformed(img)
+cv2.imwrite(img_path.replace(".jpg", "_enhaced.jpg"), img)
+
--- inference.py
+++ inference.py
@@ -79,6 +79,8 @@ def infer_one_image(model_name, image_path, box_score_thresh=0.9):
 
         
 infer_one_image("fasterRCNN_restnet", 
-                r"D:\AI\CV\CS231_Low-light-Enhancement-in-Classical-Computer-Vision-Tasks\image_test\dog_people_1.jpg")
+                r"D:\AI\CV\CS231_Low-light-Enhancement-in-Classical-Computer-Vision-Tasks\image_test\2015_02433.jpg")
+infer_one_image("fasterRCNN_restnet", 
+                r"D:\AI\CV\CS231_Low-light-Enhancement-in-Classical-Computer-Vision-Tasks\image_test\2015_02433log_transform.jpg")
 
 
